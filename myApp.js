414a152ce99527b669ad35cd35a0cb162c65f620
Cihan <cihangonen35@gmail.com>
2021-07-09 20:55:35 +0300

    env

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 console.log('Hello World');
 
@@ -9,10 +10,14 @@ app.get('/',(req,res)=>{
   res.sendFile(__dirname+'/views/index.html');
 })
 
-app.get('/json',(req,res)=>{
-  res.json({"message":"Hello json"});
-})
+app.get("/json",function(req,res){
+  if(process.env.MESSAGE_STYLE=="uppercase")
+    res.json({"message": "HELLO JSON"})
+  else 
+    res.json({"message":"Hello json"})
+});
 
+module.exports = app;
 
 
 
